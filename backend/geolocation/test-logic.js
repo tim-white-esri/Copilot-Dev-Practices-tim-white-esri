7b54b6534ac783b875103b09a97846935ea3f5ae
Tim White <tim_white@esri.com>
2023-11-09 19:36:00 +0000

    Geo Location Logic Added

--- backend/geolocation/test-logic.js
+++ backend/geolocation/test-logic.js
@@ -1,3 +1,8 @@
+/**
+ * Retrieves geolocation information for a given IP address using IP2Location library.
+ * @param {string} ipAddress - The IP address to retrieve geolocation information for.
+ * @returns {object} - An object containing geolocation information for the given IP address.
+ */
 // IP2Location - https://www.npmjs.com/package/ip2location-nodejs
 // IP2Location Node.js Library - https://ip2location-nodejs.readthedocs.io/en/latest/quickstart.html#query-geolocation-information-from-bin-database
 const {IP2Location} = require("ip2location-nodejs");
